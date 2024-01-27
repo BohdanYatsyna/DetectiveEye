e2d9aa61a4138720eb2c3291900616958077533d
BohdanYatsyna <b.yatsyna@starnavi.io>
2024-01-27 14:15:22 +0200

    Update endpoint in router.py (results_app) to work properly

--- results_app/router.py
+++ results_app/router.py
@@ -1,59 +1,107 @@
-# from fastapi import APIRouter, Depends, HTTPException, status
-# from sqlalchemy.orm import Session
-# from typing import List
-#
-# from user_app.users import current_active_user
-# from results_app import crud, schemas
-# from db.database import get_async_session
-# from user_app.models import User
-# from fastapi_users import FastAPIUsers
-#
-#
-# results_router = APIRouter()
-#
-#
-# @results_router.post("/detect_object/{video_url}", response_model=schemas.DetectionResult)
-# async def create_detection_request(video_url: str = "http://afdsgdhgjfhkl.adsfg.asfdg.sfdgh/asfd", user: User = Depends(current_active_user), db: Session = Depends(get_async_session)):
-#     task_id = "test_task_id"
-#     detection_result = schemas.DetectionResultCreate(video_url=video_url, task_id=task_id)
-#     return await crud.create_detection_result(db, detection_result, user.id)
-#
-# @results_router.get("/results/", response_model=List[schemas.DetectionResult])
-# async def read_results(user: User = Depends(current_active_user), db: Session = Depends(get_async_session)):
-#     return await crud.get_user_detection_results(db, user.id)
-#
-# @results_router.get("/results/{result_id}", response_model=schemas.DetectionResult)
-# async def read_result(result_id: int, user: User = Depends(current_active_user), db: Session = Depends(get_async_session)):
-#     db_result = await crud.get_detection_result(db, result_id)
-#     if db_result is None or db_result.user_id != user.id:
-#         raise HTTPException(status_code=404, detail="Result not found")
-#     return db_result
+import shutil
+import os
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import (
+    APIRouter, Depends, HTTPException, File, UploadFile, BackgroundTasks
+)
+from fastapi_users import FastAPIUsers
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List
+from uuid import UUID
 
-from results_app import crud, schemas
+from celery_worker.worker import process_video_task
 from db.database import get_async_session
+from results_app import crud, schemas
+from service.utils import get_video_paths, get_file_extension
+from service.video_processing import upload_video_to_temp_folder
 from user_app.models import User
 from user_app.users import current_active_user
-from fastapi_users import FastAPIUsers
+
 
 results_router = APIRouter()
 
-@results_router.post("/detect_object/{video_url}", response_model=schemas.DetectionResult)
-async def create_detection_request(video_url: str, user: User = Depends(current_active_user), db: AsyncSession = Depends(get_async_session)):
-    task_id = "test_task_id"
-    detection_result = schemas.DetectionResultCreate(video_url=video_url, task_id=task_id)
-    return await crud.create_detection_result(db, detection_result, user.id)
 
-@results_router.get("/results/", response_model=List[schemas.DetectionResult])
-async def read_results(user: User = Depends(current_active_user), db: AsyncSession = Depends(get_async_session)):
-    return await crud.get_user_detection_results(db, user.id)
+@results_router.post(
+    "/detect_objects/", response_model=schemas.DetectionResult
+)
+async def upload_video_for_detecting_objects(
+        video_file: UploadFile = File(...),
+        db: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_active_user),
+):
+    file_extension = get_file_extension(video_file)
 
-@results_router.get("/results/{result_id}", response_model=schemas.DetectionResult)
-async def read_result(result_id: int, user: User = Depends(current_active_user), db: AsyncSession = Depends(get_async_session)):
-    db_result = await crud.get_detection_result(db, result_id)
-    if db_result is None or db_result.user_id != user.id:
+    if file_extension != "mp4":
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid file type. Only '.mp4' video files are allowed."
+        )
+
+    frames_path, video_path, video_folder_path = get_video_paths()
+    upload_video_to_temp_folder(video_file, video_path)
+
+    task = process_video_task.delay(
+        frames_path, video_path, video_folder_path
+    )
+
+    new_detection_result = await crud.create_detection_result(
+        db, task_id=task.id, user_id=user.id
+    )
+
+    return new_detection_result
+
+
+@results_router.get("/results/", response_model=list[schemas.DetectionResult])
+async def read_results(
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(get_async_session)
+):
+    results_list = await crud.get_user_detection_results(db, user.id)
+
+    return results_list
+
+
+@results_router.get(
+    "/results/{task_id}", response_model=schemas.DetectionResult
+)
+async def read_result(
+        task_id: UUID,
+        user: User = Depends(current_active_user),
+        db: AsyncSession = Depends(get_async_session)
+):
+    db_result = await crud.get_detection_result(db, task_id)
+
+    if db_result is None:
         raise HTTPException(status_code=404, detail="Result not found")
+
+    elif db_result.user_id != user.id:
+        raise HTTPException(
+            status_code=400, detail="User can see only own results"
+        )
+
     return db_result
+
+
+@results_router.put(
+    "/results/{task_id}", response_model=schemas.DetectionResult
+)
+async def update_result(
+    task_id: UUID,
+    detection_result_update: schemas.DetectionResultUpdate,
+    db: AsyncSession = Depends(get_async_session),
+    user: User = Depends(current_active_user)
+):
+    existing_result = await crud.get_detection_result(db, task_id)
+    if not existing_result or existing_result.user_id != user.id:
+        raise HTTPException(
+            status_code=404,
+            detail="Result not found or not owned by the user"
+        )
+
+    updated_result = await crud.update_detection_result(
+        db, task_id, detection_result_update
+    )
+
+    if not updated_result:
+        raise HTTPException(status_code=404, detail="Result not found")
+
+    return updated_result
