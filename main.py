5b7d53a3a005ecd11e066203424bc76a673c74ca
BohdanYatsyna <b.yatsyna@starnavi.io>
2024-01-27 14:14:28 +0200

    Include results_router to main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from user_app.router import user_router
+from core.settings import settings
 from results_app.router import results_router
+from user_app.router import user_router
 
 
-app = FastAPI()
+app = FastAPI(title=settings.PROJECT_NAME)
 
 
 app.include_router(user_router)
-app.include_router(results_router)
+app.include_router(results_router, tags=["results"])
