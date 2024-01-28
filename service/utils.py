e7a4e2f41f84e21008cd6597a3fac77035ce658e
BohdanYatsyna <b.yatsyna@starnavi.io>
2024-01-28 21:03:05 +0200

    Create util functions get_file_extension, get_uuid_name, get_video_paths

---
+++ service/utils.py
@@ -0,0 +1,25 @@
+import os
+import uuid
+
+from core.settings import settings
+from fastapi import UploadFile
+
+
+def get_file_extension(file: UploadFile) -> str:
+    return file.filename.split(".")[-1].lower()
+
+
+def get_uuid_name() -> str:
+    return str(uuid.uuid4())
+
+
+def get_video_paths() -> tuple:
+    video_folder_name = get_uuid_name()
+
+    video_folder_path = os.path.join(
+        settings.MEDIA_TEMP, f"{video_folder_name}"
+    )
+    video_path = os.path.join(video_folder_path, f"{video_folder_name}.mp4")
+    frames_path = os.path.join(video_folder_path, "frames")
+
+    return frames_path, video_path, video_folder_path
