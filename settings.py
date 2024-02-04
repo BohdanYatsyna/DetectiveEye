432b8849ac2cdc9962ca6b63e0a172a9e7c7a54c
Monstradamus <bohdan.y@starnavi.io>
2024-02-04 19:17:06 +0200

    Update settings.py with configurations for Detectron2

--- settings.py
+++ settings.py
@@ -13,12 +13,29 @@ load_dotenv()
 # FastAPI settings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "DetectiveEye"
-
     DATABASE_URL: str = os.getenv("ENV_DATABASE_URL")
     ROOT_DIR: Path = Path(__file__).resolve().parent
-    TEMP_VIDEO_FOLDER: Path = os.path.join(ROOT_DIR, "temp_storage")
 
+    # Temporary storage for saving video and other files
+    TEMP_FOLDER: Path = os.path.join(ROOT_DIR, "temp_storage")
+
+    # File extension supported for objects detection
+    SUPPORTED_FILE_EXTENSIONS: list[str] = ["mp4"]
     TOKEN_SECRET: str = os.getenv("ENV_TOKEN_SECRET")
+
+    # Specifying exact Detectron2 model settings file
+    DETECTRON2_MODEL_CONFIG: str = os.getenv(
+        "ENV_DETECTRON2_MODEL_CONFIG",
+        "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+    )
+    # Set threshold for the model to show results with above prediction score
+    DETECTRON2_SCORE_THRESH_TEST: float = float(os.getenv(
+        "ENV_DETECTRON2_SCORE_THRESH_TEST", "0.7"
+    ))
+    # Choosing CPU or GPU mode
+    DETECTRON2_DEVICE: str = os.getenv(
+        "ENV_DETECTRON2_DEVICE", "cpu"
+    )
 
     class Config:
         case_sensitive = True
