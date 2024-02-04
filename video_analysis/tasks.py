bd0c7beca7246518bffcf8db639771eac4baa836
Monstradamus <bohdan.y@starnavi.io>
2024-02-04 19:18:03 +0200

    Update logic of object detection and post detection cleanup tasks in tasks.py

--- video_analysis/tasks.py
+++ video_analysis/tasks.py
@@ -1,43 +1,85 @@
-import logging
+from typing import Any
+
+from celery import Task
 
 from db.sync_database_session import get_sync_session
 from settings import celery_instance
-from video_analysis.objects_detection.detector import Detector
-from video_analysis.results.enums import DetectionStatus
-from video_analysis.results.crud import update_detection_result
-from video_analysis.utils import delete_processed_video
+from video_analysis.detection_results.enums import DetectionStatus
+from video_analysis.objects_detection.video_detectors import (
+    DEFAULT_VIDEO_OBJECTS_DETECTOR
+)
+from video_analysis.utils import delete_file
+from video_analysis.detection_results.crud import update_detection_result
 
 
-@celery_instance.task(bind=True)
-def detect_objects_on_video_task(self, video_file_path: str) -> dict:
-    detector = Detector()
-    detection_results = detector.detect_objects(video_file_path)
-
-    task_results = {
-        "task_id": self.request.id,
-        "detection_results": detection_results,
-    }
-
-    delete_processed_video(video_file_path)
-
-    return task_results
+@celery_instance.task()
+def delete_video_after_detecting_task(video_file_path: str) -> None:
+    delete_file(video_file_path)
 
 
-@celery_instance.task
-def update_detection_result_task(
-        task_results: dict
-) -> None:
+class DetectionTaskCallback(Task):
+    def after_return(
+            self,
+            status: str,
+            return_value: list,
+            task_id: str,
+            args: Any,
+            kwargs: Any,
+            einfo: Any,
+    ) -> None:
+        file_path = args[0]
+        delete_video_after_detecting_task.apply_async(args=[file_path])
 
-    with get_sync_session() as db:
-        detection_results = task_results.get("detection_results")
-        task_id = task_results.get("task_id")
-        status = DetectionStatus.SUCCESS
+    def on_success(
+            self, return_value: list, task_id: str, args: Any, kwargs: Any
+    ) -> None:
+        with get_sync_session() as db:
+            update_detection_result(
+                db, task_id, DetectionStatus.SUCCESS, return_value
+            )
 
-        if not detection_results:
-            status = DetectionStatus.FAILURE
+    def get_error_message(self, exception: Exception) -> list[str]:
+        if isinstance(exception, FileNotFoundError):
+            error_message = [
+                f"{str(exception)}. Try to upload video again"
+            ]
+        elif isinstance(exception, TimeoutError):
+            error_message = [
+                "Objects detecting take too long time, "
+                "try to upload shorter video"
+            ]
+        else:
+            error_message = [
+                "Unexpected objects detecting error, "
+                "please try to upload video again"
+            ]
 
-        update_detection_result(db, task_id, status, detection_results)
+        return error_message
 
-        logging.info(
-            f"Successfully updated DetectionResult with task_id: '{task_id}'"
+    def on_failure(
+            self,
+            exception: Exception,
+            task_id: str,
+            args: Any,
+            kwargs: Any,
+            einfo: Any,
+    ) -> None:
+        error_message = self.get_error_message(exception)
+
+        with (get_sync_session() as db):
+            update_detection_result(
+                db, task_id, DetectionStatus.FAILURE, error_message
+            )
+
+
+@celery_instance.task(
+    bind=True, base=DetectionTaskCallback
+)
+def detect_objects_on_video_task(self, video_file_path: str) -> list:
+    detection_results = (
+        DEFAULT_VIDEO_OBJECTS_DETECTOR.detect_all_objects_on_video(
+            video_file_path
         )
+    )
+
+    return detection_results
