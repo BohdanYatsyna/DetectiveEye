287e561db16cf479ed56c3da507230a6fb3ba992
Monstradamus <bohdan.y@starnavi.io>
2024-02-05 11:24:16 +0200

    Delete redundant all CV2 windows in OpenCVVideoSplitter

--- video_analysis/objects_detection/video_splitters.py
+++ video_analysis/objects_detection/video_splitters.py
@@ -13,7 +13,6 @@ class OpenCVVideoSplitter(VideoSplitter):
     def split_video_into_frames(self, video_path: str) -> bytearray:
         video_object = cv2.VideoCapture(video_path)
         if not video_object.isOpened():
-            cv2.destroyAllWindows()
             raise FileNotFoundError("Failed to open video file")
 
         try:
@@ -25,7 +24,6 @@ class OpenCVVideoSplitter(VideoSplitter):
 
         finally:
             video_object.release()
-            cv2.destroyAllWindows()
 
 
 OPENCV_VIDEO_SPLITTER = OpenCVVideoSplitter()
