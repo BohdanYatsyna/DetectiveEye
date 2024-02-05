a6111e20d90864a5431e99121d29d4c836932425
Monstradamus <bohdan.y@starnavi.io>
2024-02-05 21:14:11 +0200

    Update Detectron2FrameObjectsDetector to be more reusable

--- video_analysis/objects_detection/single_frame_detectors.py
+++ video_analysis/objects_detection/single_frame_detectors.py
@@ -1,11 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2 import model_zoo
-from detectron2.data import MetadataCatalog
-
-from settings import settings
 
 
 class FrameObjectDetector(ABC):
@@ -13,40 +9,15 @@ class FrameObjectDetector(ABC):
     def detect_objects_on_frame(
             self, frame: bytearray
     ) -> list[tuple[str | None, float | None] | None]:
-        """Detect objects on single frame"""
+        """Detect objects on single video-frame"""
 
 
 class Detectron2FrameObjectsDetector(FrameObjectDetector):
     def __init__(
-            self,
-            model_config: str = settings.DETECTRON2_MODEL_CONFIG,
-            threshold: float = settings.DETECTRON2_SCORE_THRESH_TEST,
-            device: str = settings.DETECTRON2_DEVICE
+            self, predictor: DefaultPredictor, objects_names: dict
     ) -> None:
-        # Load default Detectron2 configurations
-        self.configurations = get_cfg()
-
-        # Specifying exact Detectron2 model settings file
-        self.configurations.merge_from_file(model_zoo.get_config_file(
-            model_config
-        ))
-        self.configurations.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-            model_config
-        )
-
-        # Set threshold for the model to show results
-        # with prediction score above it
-        self.configurations.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
-
-        # Choosing CPU or GPU mode
-        self.configurations.MODEL.DEVICE = device
-
-        # Set up predictor according to configs above
-        self.predictor = DefaultPredictor(self.configurations)
-
-        # Load metadata for returning objects names in detection results
-        self.model_metadata = MetadataCatalog.get("coco_2017_train")
-        self.objects_names = self.model_metadata.get("thing_classes")
+        self.predictor = predictor
+        self.objects_names = objects_names
 
     def detect_objects_on_frame(
             self, frame: bytearray
@@ -54,14 +25,13 @@ class Detectron2FrameObjectsDetector(FrameObjectDetector):
         # Convert frame to RGB
         frame_rgb = frame[:, :, ::-1]
 
-        outputs = self.predictor(frame_rgb)
-        predicted_classes = outputs["instances"].pred_classes.tolist()
-        prediction_scores = outputs["instances"].scores.tolist()
+        detection_outputs = self.predictor(frame_rgb)
+        predicted_classes = detection_outputs[
+            "instances"
+        ].pred_classes.tolist()
+        prediction_scores = detection_outputs["instances"].scores.tolist()
 
         return [
             (self.objects_names[name], round(score, 4))
             for name, score in zip(predicted_classes, prediction_scores)
         ]
-
-
-DETECTRON2_FRAMES_DETECTOR = Detectron2FrameObjectsDetector()
