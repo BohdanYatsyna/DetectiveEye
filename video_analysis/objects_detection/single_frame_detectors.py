8724e0ab6471702f8f3c67cb47b42f470fd54929
Monstradamus <bohdan.y@starnavi.io>
2024-02-06 14:57:16 +0200

    Create YOLOv8FrameObjectsDetector and in single_frame_detectors.py.py, add additional comments

--- video_analysis/objects_detection/single_frame_detectors.py
+++ video_analysis/objects_detection/single_frame_detectors.py
@@ -8,11 +8,13 @@ class FrameObjectDetector(ABC):
     @abstractmethod
     def detect_objects_on_frame(
             self, frame: bytearray
-    ) -> list[tuple[str | None, float | None] | None]:
+    ) -> list[list[str | None, float | None] | None]:
         """Detect objects on single video-frame"""
 
 
 class Detectron2FrameObjectsDetector(FrameObjectDetector):
+    """Facebook Detectron2 single frame/image objects detector"""
+
     def __init__(
             self, predictor: DefaultPredictor, objects_names: dict
     ) -> None:
@@ -34,4 +36,36 @@ class Detectron2FrameObjectsDetector(FrameObjectDetector):
         return [
             (self.objects_names[name], round(score, 4))
             for name, score in zip(predicted_classes, prediction_scores)
+        ]
+
+
+class YOLOv8FrameObjectsDetector(FrameObjectDetector):
+    """Ultralytics YOLOv8 single frame/image objects detector"""
+
+    def __init__(
+            self, model: Any, threshold: float, device: str
+    ) -> None:
+        self.model = model
+        self.threshold = threshold
+        self.device = device
+
+    def detect_objects_on_frame(
+            self, frame: bytearray
+    ) -> list[tuple[str | None, float | None] | None]:
+
+        raw_detection_outputs = self.model.predict(
+            frame, conf=self.threshold, device=self.device
+        )
+
+        objects_names_dict = self.model.names
+
+        detection_outputs = raw_detection_outputs[0].numpy()
+        predicted_objects_ids = detection_outputs.boxes.cls.tolist()
+        prediction_scores = detection_outputs.boxes.conf.tolist()
+
+        return [
+            (objects_names_dict[int(predicted_object_id)], round(score, 4))
+            for predicted_object_id, score in zip(
+                predicted_objects_ids, prediction_scores
+            )
         ]
