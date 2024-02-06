a06cb1f8d4f35b3d57c6257eb53613d4e873955b
Monstradamus <bohdan.y@starnavi.io>
2024-02-06 14:59:56 +0200

    Create ObjectsDetectorChoice to use it, as instrument for choosing objects detector

--- video_analysis/detection_results/enums.py
+++ video_analysis/detection_results/enums.py
@@ -5,3 +5,8 @@ class DetectionStatus(Enum):
     PROCESSING = "Processing"
     FAILURE = "Failure"
     SUCCESS = "Success"
+
+
+class ObjectsDetectorChoice(Enum):
+    DEFAULT_DETECTRON2 = "Detectron2"
+    DEFAULT_YOLOv8 = "YOLOv8"
